20e0d135a5337b08cd7d46fca21afa18140bae09
golamarihanimireddy10 <59246512+golamarihanimireddy10@users.noreply.github.com>
2024-10-16 16:55:30 +0100

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Name of the Docker container that needs to be stopped
+CONTAINER_NAME="flask_app_container"
+
+# Check if the container is running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping running container: $CONTAINER_NAME"
+    
+    # Stop the container
+    docker stop $CONTAINER_NAME
+    
+    # Optionally remove the container if you want to clean up
+    docker rm $CONTAINER_NAME
+    
+    echo "Container $CONTAINER_NAME stopped and removed."
+else
+    echo "No running container named $CONTAINER_NAME found."
+fi
